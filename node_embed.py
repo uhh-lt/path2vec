4edc1ff19392037161a5849fe7f4a8f6590f6376
Alexander Panchenko <panchenko.alexander@gmail.com>
2018-03-15 18:41:32 +0100

    formatting of the code

--- node_embed.py
+++ node_embed.py
@@ -21,6 +21,7 @@ import argparse
 DIM = 5
 NUM_SAMPLES = 4
 NUM_ITER = 600
+DEVICE = '/cpu:0'
 
 
 def read_edges(filename):
@@ -30,40 +31,52 @@ def read_edges(filename):
 
 
 def build_dataset(graph):
+    """ Load the data from a networkx graph. """
     index = 0
     number_of_edges = graph.number_of_edges()
     dataset = np.ndarray(shape=(number_of_edges), dtype=np.int32)
     labels = np.ndarray(shape=(number_of_edges, 1), dtype=np.int32)
     dictionary = {k: v for v, k in enumerate(graph.nodes)}
     reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    
     for e in graph.edges:
         dataset[index] = dictionary[e[0]]
         labels[index] = dictionary[e[1]]
         index = index+1
+
     return dataset, labels, dictionary, reverse_dictionary
 
 
 def plot(embeddings, labels):
+    """ Plot the obtained embeddings """ 
+
     assert embeddings.shape[0] >= len(labels), 'More labels than embeddings'
     pylab.figure(figsize=(15,15))  # in inches
+
     for i, label in enumerate(labels):
         x, y = embeddings[i,:]
         pylab.scatter(x, y)
         pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+
     pylab.show()
 
 
-def run(filename, embedding_size=DIM, num_sampled=NUM_SAMPLES, num_steps=NUM_ITER,
+def run(graph_fpath, embedding_size=DIM, num_sampled=NUM_SAMPLES, num_steps=NUM_ITER,
      valid_size=3, num_points=20, batch_size=120):
+
+     """ Train graph embeddings """
     
+    # Load the data
     valid_examples = np.array(random.sample(range(10), valid_size))
-    G = read_edges(filename)
+    G = read_edges(graph_fpath)
     node_size = G.number_of_nodes()
     edges_size = G.number_of_edges()
-    print("Number of nodes: %d " %node_size)
-    data,labels,dictionary,rdictionary=build_dataset(G)
+    print("Number of nodes: {}".format(node_size))
+    data, labels, dictionary, rdictionary = build_dataset(G)
+
+    # Construct the computational graph
     graph = tf.Graph()
-    with graph.as_default(), tf.device('/cpu:0'):
+    with graph.as_default(), tf.device(DEVICE):
         train_dataset = tf.placeholder(tf.int32, shape=[edges_size])
         train_labels = tf.placeholder(tf.int32, shape=[edges_size, 1])
         valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
@@ -74,22 +87,23 @@ def run(filename, embedding_size=DIM, num_sampled=NUM_SAMPLES, num_steps=NUM_ITE
             stddev=1.0 / math.sqrt(embedding_size)))
         softmax_biases = tf.Variable(tf.zeros([node_size]))
         embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+        
         loss = tf.reduce_mean(
-            tf.nn.sampled_softmax_loss(\
-                    weights=softmax_weights,\
-                    biases=softmax_biases,\
-                    inputs=embed,\
-                    labels=train_labels,\
-                    num_sampled=num_sampled,\
+            tf.nn.nce_loss(
+                    weights=softmax_weights,
+                    biases=softmax_biases,
+                    inputs=embed,
+                    labels=train_labels,
+                    num_sampled=num_sampled,
                     num_classes=node_size))
+        
         optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
-        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, 
-            keep_dims=True))
+        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
         normalized_embeddings = embeddings / norm
-        valid_embeddings = tf.nn.embedding_lookup(
-        normalized_embeddings, valid_dataset)
-        similarity = tf.matmul(valid_embeddings,tf.transpose(normalized_embeddings))
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
 
+    # Perform the computation
     with tf.Session(graph=graph) as session:
         tf.initialize_all_variables().run()
         print('Variables Initialized......')
@@ -97,14 +111,14 @@ def run(filename, embedding_size=DIM, num_sampled=NUM_SAMPLES, num_steps=NUM_ITE
         for step in range(num_steps):
             feed_dict = {train_dataset : data, train_labels : labels}
             _, l = session.run([optimizer, loss], feed_dict=feed_dict)
-            print('Average loss at step %d: %f' % (step, l))
-            if step % 100== 0:
+            print('Average loss at step {}: {}'.format(step, l))
+            if step % 100 == 0:
                 sim = similarity.eval()
                 for i in range(valid_size):
                     valid_word = rdictionary[valid_examples[i]]
                     top_k = 8 # number of nearest neighbors
                     nearest = (-sim[i, :]).argsort()[1:top_k+1]
-                    log = 'Nearest to %s:' % valid_word
+                    log = 'Nearest to {}:'.format(valid_word)
                     for k in range(top_k):
                         close_word = rdictionary[nearest[k]]
                         log = '%s %s,' % (log, close_word)
@@ -115,8 +129,6 @@ def run(filename, embedding_size=DIM, num_sampled=NUM_SAMPLES, num_steps=NUM_ITE
 
     words = [rdictionary[i] for i in range(1, num_points+1)]
     plot(two_d_embeddings, words)
-
-
 
 
 def main():
