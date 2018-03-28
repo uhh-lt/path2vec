01550893f4ae4b38689a9fd71ddbfed5a80f1a48
Andrey Kutuzov <akutuzov72@gmail.com>
2018-03-28 19:37:20 +0200

    Final code for calculating Wordnet similarity.

--- compute_paths.py
+++ compute_paths.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import sys
 from itertools import combinations
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic
@@ -10,18 +11,20 @@ ic = wordnet_ic.ic('ic-semcor.dat')
 
 synsets = list(wn.all_synsets('n'))
 
+print('Total synsets:', len(synsets), file=sys.stderr)
 
-print(len(synsets))
+pairs = len(synsets)**2
 
 counter = 0
 
 start = time.time()
 for el in combinations(synsets, 2):
-    # similarity = el[0].jcn_similarity(el[1], ic) #  Jiang-Conrath
-    similarity = el[0].lch_similarity(el[1])  # Leacock-Chodorow
+    similarity = el[0].jcn_similarity(el[1], ic)  # Jiang-Conrath
+    # similarity = el[0].lch_similarity(el[1])  # Leacock-Chodorow
+    print(el[0].name() + '\t' + el[1].name() + '\t', similarity)
     counter += 1
-    if counter > 100000:
-        break
+    if counter % 100000 == 0:
+        print(counter, 'out of', pairs, file=sys.stderr)
+
 end = time.time()
-print(end - start)
-print(counter)
+print('Total time spent:', end - start)
