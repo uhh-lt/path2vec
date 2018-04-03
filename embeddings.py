a946cbe39b60342e1f64b23fe9c8df44a1d194df
Andrey Kutuzov <akutuzov72@gmail.com>
2018-04-03 23:15:22 +0200

    Generating embeddings over wordnet paths.

--- embeddings.py
+++ embeddings.py
@@ -1,40 +1,140 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-from keras.engine import Input
+from keras import backend as K
+import numpy as np
+from keras import Input
 from keras.models import Model
 from keras.layers.merge import dot
-import gensim
-import logging
+from keras.layers.embeddings import Embedding
+from keras.layers import Lambda
+import gzip
+import random
 import sys
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-m = sys.argv[1]
+class Wordpairs(object):
+    """docstring for Wordpairs"""
 
-# Detecting the model format
-if m.endswith('.vec.gz'):
-    model = gensim.models.KeyedVectors.load_word2vec_format(m, binary=False)
-elif m.endswith('.bin.gz'):
-    model = gensim.models.KeyedVectors.load_word2vec_format(m, binary=True, encoding='utf8')
-else:
-    model = gensim.models.Word2Vec.load(m)
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-wv = model.wv
-embedding_layer = wv.get_keras_embedding()
-
-print(embedding_layer)
+    def __iter__(self):
+        with gzip.open(self.filepath, "rb") as rf:
+            for line in rf:
+                if line.strip():
+                    yield line.strip().decode('utf-8')
 
 
-input_a = Input(shape=(1,), dtype='int32', name='input_a')
-input_b = Input(shape=(1,), dtype='int32', name='input_b')
-embedding_a = embedding_layer(input_a)
-embedding_b = embedding_layer(input_b)
-similarity = dot([embedding_a, embedding_b], axes=2, normalize=True)
+def build_vocabulary(pairs):
+    # Generating Vocabulary from the sentences
+    # Count the total number of training words
+    vocab = {}
+    train_words = 0
+    for pair in pairs:
+        (word0, word1, similarity) = pair.strip().split('\t')
+        for word in [word0, word1]:
+            vocab.setdefault(word, 0)
+            vocab[word] += 1
+            train_words += 1
+    print('Vocabulary size = %d' % len(vocab), file=sys.stderr)
+    print('Total words to be trained = %d' % train_words, file=sys.stderr)
 
-keras_model = Model(inputs=[input_a, input_b], outputs=similarity)
-keras_model.compile(optimizer='sgd', loss='mse')
+    sorted_words = reversed(sorted(vocab, key=lambda w: vocab[w]))
+    reverse_vocab = {}
+    index_val = 0
+    for word in sorted_words:
+        reverse_vocab[word] = index_val
+        index_val += 1
 
+    return vocab, train_words, reverse_vocab
+
+
+def batch_generator(pairs, reverse_vocab):
+    # Restart running from the first sentence if the the file reading is done
+    while True:
+        # Read one sentence from the file into memory
+        # For each sentence generate word index sequence
+        # Now spit out batches for training
+        # Each batch will emit the following things
+        # 1 - current word index
+        # 2 - context word indexes
+        # 3 - negative sampled word indexes
+        for pair in pairs:
+            # split the sentence on whitespace
+            words = pair.strip().split('\t')[:2]
+            sim = float(pair.strip().split('\t')[2])
+
+            sent_seq = [reverse_vocab[word] for word in words]
+
+            # Create current batch
+            current_word_index = np.array([sent_seq[0]])
+            context_word_index = np.array([sent_seq[1]])
+            # get negative samples
+            neg_samples = get_negative_samples(current_word_index)
+            # yield a batch here
+            # batch should be a tuple of inputs and targets
+            yield [current_word_index, context_word_index, neg_samples], [np.array([sim]), np.zeros((1, negative))]
+
+
+def get_negative_samples(current_word_index):
+    # Generate random negative samples
+    neg_samples = random.sample(range(vocab_size), negative)
+    while current_word_index in neg_samples:
+        neg_samples = random.sample(range(vocab_size), negative)
+    return np.array([neg_samples])
+
+
+embedding_dimension = 100
+min_count = 2
+window_size = 1
+negative = 1
+
+trainfile = sys.argv[1]
+
+wordpairs = Wordpairs(trainfile)
+vocabulary, no_train_words, reverse_vocabulary = build_vocabulary(wordpairs)
+vocab_size = len(vocabulary)
+
+# generate embedding matrices with all values between -1/2d, 1/2d
+w_embedding = np.random.uniform(-0.5 / embedding_dimension, 0.5 / embedding_dimension,
+                                (vocab_size, embedding_dimension))
+c_embedding = np.random.uniform(-0.5 / embedding_dimension, 0.5 / embedding_dimension,
+                                (vocab_size, embedding_dimension))
+
+# Model has 3 inputs
+# Current word index, context words indexes and negative sampled word indexes
+word_index = Input(shape=(1,), name='Word')
+context_index = Input(shape=(1,), name='Context')
+negative_samples = Input(shape=(negative,), name='Negative')
+
+# All the inputs are processed through embedding layers
+word_embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_dimension, weights=[w_embedding],
+                                 name='Word_embeddings')
+context_embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_dimension, weights=[c_embedding],
+                                    name='Context_embeddings')
+word_embedding = word_embedding_layer(word_index)
+context_embedding = context_embedding_layer(context_index)
+negative_words_embedding = context_embedding_layer(negative_samples)
+
+final_context = Lambda(lambda x: K.mean(x, axis=1), output_shape=(embedding_dimension,),
+                       name='final_context_embedding')(context_embedding)
+
+final_ns = Lambda(lambda x: K.mean(x, axis=1), output_shape=(embedding_dimension,),
+                  name='final_ns_embedding')(negative_words_embedding)
+
+# The context is multiplied (dot product) with current word and negative sampled words
+word_context_product = dot([word_embedding, final_context], axes=-1, normalize=True,
+                           name='word2context')
+negative_context_product = dot([word_embedding, final_ns], axes=-1, normalize=True,
+                               name='word2negative')
+
+keras_model = Model(inputs=[word_index, context_index, negative_samples],
+                    outputs=[word_context_product, negative_context_product])
+
+# binary crossentropy is applied on the output
+keras_model.compile(optimizer='adagrad', loss='binary_crossentropy')
 print(keras_model.summary())
 
-
+keras_model.fit_generator(batch_generator(wordpairs, reverse_vocabulary),
+                          steps_per_epoch=no_train_words, epochs=10, workers=2)
