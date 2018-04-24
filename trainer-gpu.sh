a3d0e26039e878d527e2b365b248afec7619744b
Alexander Panchenko <panchenko.alexander@gmail.com>
2018-04-24 09:19:38 -0700

    simplify the trainer gpu

--- trainer-gpu.sh
+++ trainer-gpu.sh
@@ -1,9 +1,8 @@
+export CUDA_VISIBLE_DEVICES=$3
 dataset=$1
 vsize=$2
-export CUDA_VISIBLE_DEVICES=$3
+bsize=$4 
 
-for lrate in 0.001 0.002 0.0005 0.004 ; do
-	for bsize in 100 200 50 25 10 ; do
-		python embeddings.py ${dataset} ${vsize} ${bsize} ${lrate} synsets_vocab.json.gz
-	done
+for lrate in 0.005 ; do
+	python embeddings.py ${dataset} ${vsize} ${bsize} ${lrate} synsets_vocab.json.gz
 done
