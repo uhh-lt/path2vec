d4ef1eb4cf120868f4d55b018a1c88c859f1f14d
Andrey Kutuzov <akutuzov72@gmail.com>
2018-04-26 19:41:12 +0200

    Evaluation script

---
+++ evaluation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import gensim
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# Loading model and semantic similarity dataset
+modelfile, simfile = sys.argv[1:]
+
+model = gensim.models.KeyedVectors.load_word2vec_format(modelfile, binary=False)
+
+# Pre-calculating vector norms
+model.init_sims(replace=True)
+
+a = model.evaluate_word_pairs(simfile, dummy4unknown=True)
+
+name = modelfile.split('.')[0]
+print(name+'\t'+str(a[1][0]))
+
