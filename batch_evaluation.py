4ea4791e36d424a5464b09d4c2675302a1577a30
Andrey Kutuzov <akutuzov72@gmail.com>
2018-04-27 16:40:24 +0200

    Updated scores on more models

---
+++ batch_evaluation.py
@@ -0,0 +1,35 @@
+#!/projects/ltg/python3/bin/python3
+import gensim
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# Loading model and semantic similarity dataset
+modelfile = sys.argv[1]
+
+model = gensim.models.KeyedVectors.load_word2vec_format(modelfile, binary=False)
+
+# Pre-calculating vector norms
+model.init_sims(replace=True)
+
+if 'jcn-semcor' in modelfile:
+    simfile_wordnet = 'simlex/simlex_jcn_semcor.tsv'
+    simfile_humans = 'simlex/simlex_synsets/max_jcn_semcor_human.tsv'
+elif 'jcn-brown' in modelfile:
+    simfile_wordnet = 'simlex/simlex_jcn_brown.tsv'
+    simfile_humans = 'simlex/simlex_synsets/max_jcn_brown_human.tsv'
+elif 'lch' in modelfile:
+    simfile_wordnet = 'simlex/simlex_lch.tsv'
+    simfile_humans = 'simlex/simlex_synsets/max_lch_human.tsv'
+
+wordnet_corr = model.evaluate_word_pairs(simfile_wordnet, dummy4unknown=True)
+humans_corr = model.evaluate_word_pairs(simfile_humans, dummy4unknown=True)
+
+name = modelfile.split('/')[1].replace('_embeddings_',
+'_')[:-7].replace('vsize','').replace('bsize', '').replace('_lr', '_0.')
+name = '\t'.join(name.split('_'))
+output = [name, str(wordnet_corr[1][0]), str(humans_corr[1][0])]
+
+print('\t'.join(output))
+
