e5069d9505c3f0ad83eb93a24d2bc541a9c3bff1
Mohammad Dorgham <5dorgham@ltgpu2.informatik.uni-hamburg.de>
2018-07-26 13:51:30 +0200

    regularizer version

---
+++ embeddings_2.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import multiprocessing
+from keras import Input, losses
+from keras.models import Model
+from keras.layers.merge import dot
+from keras.layers.embeddings import Embedding
+from keras.layers import Flatten
+from keras import optimizers
+from keras import backend
+from keras.callbacks import TensorBoard, EarlyStopping
+from helpers import *
+from tensorflow.python.client import device_lib
+from nltk.corpus import wordnet as wn
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+
+print(device_lib.list_local_devices())
+neighbors_dict = dict()
+
+def build_connections(vocab_dict):
+    neighbors_dict = dict()
+    neighbor_nodes = []
+    for vocab, index in vocab_dict.items():
+        if len(vocab.lower().rsplit('.', 2)) != 3:
+            continue
+        synset = wn.synset(vocab)
+        hypernyms = synset.hypernyms()
+        hyponyms = synset.hyponyms()
+        
+        for hypernym in hypernyms:
+            if vocab_dict[hypernym.name()]:
+                neighbor_nodes.append(vocab_dict[hypernym.name()])
+        for hyponym in hyponyms:
+            if vocab_dict[hyponym.name()]:
+                neighbor_nodes.append(vocab_dict[hyponym.name()])
+        
+        neighbors_dict[index] = neighbor_nodes
+        neighbor_nodes = []
+    
+    return neighbors_dict
+    
+def customLoss(y_true, y_pred):
+    m_loss = K.mean(K.square(y_pred - y_true), axis=-1)
+    in0_unpacked = tf.unstack(keras_model.inputs[0])
+    for t in in0_unpacked:
+        nbr_list = neighbors_dict[K.eval(t)]
+        prdction=0
+        for nbr in nbr_list:
+            nbr = K.placeholder([nbr], dtype='int32')
+            prdction += validation_model.predict([t, nbr])
+    m_loss += 10e-7 * prdction
+    
+    in1_unpacked = tf.unstack(keras_model.inputs[1])
+    for t in in1_unpacked:
+        nbr_list = neighbors_dict[K.eval(t)]
+        prdction=0
+        for nbr in nbr_list:
+            nbr = K.placeholder([nbr], dtype='int32')
+            prdction += validation_model.predict([t, nbr])
+    m_loss += 10e-7 * prdction
+    
+    return m_loss
+
+
+# This script trains word embeddings on pairs of words and their similarities.
+# A possible source of such data is Wordnet and its shortest paths.
+
+# Arguments:
+
+# [obligatory] tab-separated gzipped file with training pairs and their similarities:
+# person.n.01     lover.n.03       0.22079177574204348
+# etc...
+
+# [obligatory] vector size (20, 40, 100, 300, etc)
+
+# [obligatory] batch size (10, 20, 60, 100 etc)
+
+# [obligatory] learning rate (default for Adam is 0.001)
+
+# [optional] gzipped JSON file with the vocabulary (list of words):
+# ["UNK", "'hood.n.01", "1530s.n.01", "15_may_organization.n.01", "1750s.n.01", "1760s.n.01"...]
+# etc
+# If the vocabulary file is not provided, it will be inferred from the training set
+# (can be painfully slow for large datasets)
+
+trainfile = sys.argv[1]  # Gzipped file with pairs and their similarities
+embedding_dimension = int(sys.argv[2])  # vector size (for example, 20)
+batch_size = int(sys.argv[3])  # number of pairs in a batch (for example, 10)
+learn_rate = float(sys.argv[4])   # Learning rate
+
+negative = 3  # number of negative samples
+cores = multiprocessing.cpu_count()
+
+wordpairs = Wordpairs(trainfile)
+
+if len(sys.argv) < 6:
+    print('Building vocabulary from the training set...', file=sys.stderr)
+    no_train_pairs, vocab_dict, vocab_list = build_vocabulary(wordpairs)
+    print('Building vocabulary finished', file=sys.stderr)
+else:
+    vocabulary_file = sys.argv[5]  # JSON file with the ready-made vocabulary
+    print('Loading vocabulary from file', vocabulary_file, file=sys.stderr)
+    vocab_dict, vocab_list = vocab_from_file(vocabulary_file)
+    print('Counting the number of pairs in the training set...')
+    no_train_pairs = 0
+    for line in wordpairs:
+        no_train_pairs += 1
+    print('Number of pairs in the training set:', no_train_pairs)
+
+neighbors_dict = build_connections(vocab_dict)
+
+vocab_size = len(vocab_dict)
+valid_size = 4  # Number of random words to log their nearest neighbours after each epoch
+# valid_examples = np.random.choice(vocab_size, valid_size, replace=False)
+
+# But for now we will just use a couple of known pairs to log their similarities:
+# Gold similarities:
+# measure.n.02    fundamental_quantity.n.01        0.930846519882644
+# person.n.01     lover.n.03       0.22079177574204348
+valid_examples = ['measure.n.02', 'fundamental_quantity.n.01', 'person.n.01', 'lover.n.03']
+
+# TODO: how to properly initialize weights?
+# generate embedding matrix with all values between -0.5d, 0.5d
+# w_embedding = np.random.uniform(
+# -0.5 / embedding_dimension, 0.5 / embedding_dimension, (vocab_size, embedding_dimension))
+# word_embedding_layer = Embedding(
+# input_dim=vocab_size, output_dim=embedding_dimension, weights=[w_embedding], name='Word_embeddings')
+
+# Model has 2 inputs: current word index, context word index
+word_index = Input(shape=(1,), name='Word', dtype='int32')
+context_index = Input(shape=(1,), name='Context', dtype='int32')
+
+# For now, let's use Keras defaults for initialization:
+word_embedding_layer = Embedding(vocab_size, embedding_dimension, input_length=1, name='Word_embeddings')
+
+# All the inputs are processed through the embedding layer
+word_embedding = word_embedding_layer(word_index)
+word_embedding = Flatten(name='word_vector')(word_embedding)  # Some Keras black magic for reshaping
+context_embedding = word_embedding_layer(context_index)
+context_embedding = Flatten(name='context_vector')(context_embedding)  # Some Keras black magic for reshaping
+
+# The current word embedding is multiplied (dot product) with the context word embedding
+# TODO: what about negative dot products? Insert sigmoid...?
+word_context_product = dot([word_embedding, context_embedding], axes=1, normalize=True, name='word2context')
+
+# Creating the model itself...
+keras_model = Model(inputs=[word_index, context_index], outputs=[word_context_product])
+
+# Assigning attributes:
+keras_model.vexamples = valid_examples
+keras_model.ivocab = vocab_list
+keras_model.vsize = vocab_size
+
+# create a secondary validation model to run our similarity checks during training
+word_index_v = Input(shape=(1,), name='Word_v', dtype='int32')
+context_index_v = Input(shape=(1,), name='Context_v', dtype='int32')
+word_embedding_v = word_embedding_layer(word_index_v)
+word_embedding_v = Flatten(name='word_vector')(word_embedding_v)  
+context_embedding_v = word_embedding_layer(context_index_v)
+context_embedding_v = Flatten(name='context_vector')(context_embedding_v) 
+similarity = dot([word_embedding_v, context_embedding_v], axes=1, normalize=True)
+validation_model = Model(inputs=[word_index_v, context_index_v], outputs=[similarity])
+
+# TODO:  increase the batch size during training, as in https://openreview.net/pdf?id=B1Yy1BxCZ ?
+
+adam = optimizers.Adam(lr=learn_rate)
+
+keras_model.compile(optimizer=adam, loss=customLoss, metrics=['mse'])
+
+print(keras_model.summary())
+print('Batch size:', batch_size)
+
+train_name = trainfile.split('.')[0] + '_embeddings_vsize' \
+             + str(embedding_dimension)+'_bsize'+str(batch_size)+'_lr'+str(learn_rate).split('.')[-1]
+
+
+sim_cb = SimilarityCallback(validation_model=validation_model)
+
+loss_plot = TensorBoard(log_dir=train_name + '_logs', write_graph=False, embeddings_freq=10)
+earlystopping = EarlyStopping(monitor='loss', min_delta=0.0001, patience=1, verbose=1, mode='auto')
+
+steps = no_train_pairs / batch_size  # How many times per epoch we will ask the batch generator to yield a batch
+
+# Let's start training!
+start = time.time()
+history = keras_model.fit_generator(batch_generator(wordpairs, vocab_dict, vocab_size, negative, batch_size),
+                                    callbacks=[sim_cb, loss_plot, earlystopping], steps_per_epoch=steps, epochs=10,
+                                    workers=cores, verbose=1)
+
+end = time.time()
+print('Training took:', int(end - start), 'seconds', file=sys.stderr)
+
+# Saving the resulting vectors:
+filename = train_name + '.vec.gz'
+save_word2vec_format(filename, vocab_dict, word_embedding_layer.get_weights()[0])
+
+backend.clear_session()
