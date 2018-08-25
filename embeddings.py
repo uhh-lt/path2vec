86041a9e9b01cae9175e70064c3604dce134cc2e
Mohammad Dorgham <5dorgham@ltgpu2.informatik.uni-hamburg.de>
2018-08-25 12:35:00 +0200

    merging embeddings_2 with embeddings

--- embeddings.py
+++ embeddings.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import multiprocessing
-from keras import Input
+from keras import Input, losses
 from keras.models import Model
 from keras.layers.merge import dot
 from keras.layers.embeddings import Embedding
@@ -12,8 +12,39 @@ from keras import backend
 from keras.callbacks import TensorBoard, EarlyStopping
 from helpers import *
 from tensorflow.python.client import device_lib
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+import random as rn
+
+#fix seeds for repeatability of experiments
+np.random.seed(42)
+rn.seed(12345)
+tf.set_random_seed(2)
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+neighbors_count = 3
 
 print(device_lib.list_local_devices())
+
+
+def custom_loss(reg_1_output, reg_2_output):
+    def my_loss(y_true, y_pred):
+        if len(reg_1_output)>0 and len(reg_2_output)>0:
+            beta = 0.01
+            gamma = 0.01
+            alpha = 1 - (beta+gamma)
+            m_loss = alpha * K.mean(K.square(y_pred - y_true), axis=-1)
+            
+            m_loss -= beta * (sum(reg_1_output) / float(len(reg_1_output)))
+            m_loss -= gamma * (sum(reg_2_output) / float(len(reg_2_output)))
+        else:
+            m_loss = K.mean(K.square(y_pred - y_true), axis=-1)
+    
+        return m_loss
+    return my_loss
 
 # This script trains word embeddings on pairs of words and their similarities.
 # A possible source of such data is Wordnet and its shortest paths.
@@ -48,19 +79,21 @@ wordpairs = Wordpairs(trainfile)
 
 if len(sys.argv) < 6:
     print('Building vocabulary from the training set...', file=sys.stderr)
-    no_train_pairs, vocabulary, inverted_vocabulary = build_vocabulary(wordpairs)
+    no_train_pairs, vocab_dict, inverted_vocabulary = build_vocabulary(wordpairs)
     print('Building vocabulary finished', file=sys.stderr)
 else:
     vocabulary_file = sys.argv[5]  # JSON file with the ready-made vocabulary
     print('Loading vocabulary from file', vocabulary_file, file=sys.stderr)
-    vocabulary, inverted_vocabulary = vocab_from_file(vocabulary_file)
+    vocab_dict, inverted_vocabulary = vocab_from_file(vocabulary_file)
     print('Counting the number of pairs in the training set...')
     no_train_pairs = 0
     for line in wordpairs:
         no_train_pairs += 1
     print('Number of pairs in the training set:', no_train_pairs)
 
-vocab_size = len(vocabulary)
+neighbors_dict = build_connections(vocab_dict)
+
+vocab_size = len(vocab_dict)
 valid_size = 4  # Number of random words to log their nearest neighbours after each epoch
 # valid_examples = np.random.choice(vocab_size, valid_size, replace=False)
 
@@ -84,18 +117,41 @@ word_embedding_layer = Embedding(vocab_size, embedding_dimension, input_length=1
 word_index = Input(shape=(1,), name='Word')
 context_index = Input(shape=(1,), name='Context')
 
+w_neighbors_indices = []
+c_neighbors_indices = []
+
+for n in range(neighbors_count):
+    w_neighbors_indices.append(Input(shape=(1,), dtype='int32'))
+    c_neighbors_indices.append(Input(shape=(1,), dtype='int32'))
+
 # All the inputs are processed through the embedding layer
 word_embedding = word_embedding_layer(word_index)
 word_embedding = Flatten(name='word_vector')(word_embedding)  # Some Keras black magic for reshaping
 context_embedding = word_embedding_layer(context_index)
 context_embedding = Flatten(name='context_vector')(context_embedding)  # Some Keras black magic for reshaping
+w_neighbor_embeds = []
+c_neighbor_embeds = []
+for n in range(neighbors_count):
+    w_neighbor_embeds.append(Flatten()(word_embedding_layer(w_neighbors_indices[n])))
+    c_neighbor_embeds.append(Flatten()(word_embedding_layer(c_neighbors_indices[n])))
 
 # The current word embedding is multiplied (dot product) with the context word embedding
 # TODO: what about negative dot products? Insert sigmoid...?
 word_context_product = dot([word_embedding, context_embedding], axes=1, normalize=True, name='word2context')
 
+reg1_output = []
+reg2_output = []
+for n in range(neighbors_count):
+    reg1_output.append(dot([word_embedding, w_neighbor_embeds[n]], axes=1, normalize=True))
+    reg2_output.append(dot([context_embedding, c_neighbor_embeds[n]], axes=1, normalize=True))
+
+inputs_list = [word_index, context_index]
+for i in range(neighbors_count):
+    inputs_list.append(w_neighbors_indices[i])
+for i in range(neighbors_count):
+    inputs_list.append(c_neighbors_indices[i])
 # Creating the model itself...
-keras_model = Model(inputs=[word_index, context_index], outputs=[word_context_product])
+keras_model = Model(inputs= inputs_list, outputs=[word_context_product])
 
 # Assigning attributes:
 keras_model.vexamples = valid_examples
@@ -106,7 +162,7 @@ keras_model.vsize = vocab_size
 
 adam = optimizers.Adam(lr=learn_rate)
 
-keras_model.compile(optimizer=adam, loss='mean_squared_error', metrics=['mse'])
+keras_model.compile(optimizer=adam, loss=custom_loss(reg1_output, reg2_output), metrics=['mse'])
 
 print(keras_model.summary())
 print('Batch size:', batch_size)
@@ -126,7 +182,7 @@ steps = no_train_pairs / batch_size  # How many times per epoch we will ask the 
 
 # Let's start training!
 start = time.time()
-history = keras_model.fit_generator(batch_generator(wordpairs, vocabulary, vocab_size, negative, batch_size),
+history = keras_model.fit_generator(batch_generator(wordpairs, vocab_dict, vocab_size, negative, batch_size, neighbors_count),
                                     callbacks=[sim_cb, loss_plot, earlystopping], steps_per_epoch=steps, epochs=10,
                                     workers=cores, verbose=1)
 
@@ -135,6 +191,6 @@ print('Training took:', int(end - start), 'seconds', file=sys.stderr)
 
 # Saving the resulting vectors:
 filename = train_name + '.vec.gz'
-save_word2vec_format(filename, vocabulary, word_embedding_layer.get_weights()[0])
+save_word2vec_format(filename, vocab_dict, word_embedding_layer.get_weights()[0])
 
 backend.clear_session()
