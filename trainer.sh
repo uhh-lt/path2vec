42414397dc4281c92de9b5fbd3c5373f45c24239
Mohammad Dorgham <muhammad.dorgham@gmail.com>
2018-10-01 18:38:10 +0200

    Update trainer.sh

--- trainer.sh
+++ trainer.sh
@@ -9,8 +9,8 @@ for vsize in 16 32 64 100 200 300
 	    # Learning rates
 	    for lrate in 0.0005 0.001 0.002 0.004
 		do
-		    python3 embeddings.py jcn_semcor_thresh01.tsv.gz  ${vsize} ${bsize} ${lrate} synsets_vocab.json.gz
-		    python3 embeddings.py jcn_brown_thresh01.tsv.gz  ${vsize} ${bsize} ${lrate} synsets_vocab.json.gz
+		    python3 embeddings.py --input_file jcn_semcor_thresh01.tsv.gz  --vsize ${vsize} --bsize ${bsize} --lrate ${lrate} --vocab_file synsets_vocab.json.gz --use_neighbors True --neighbor_count 3 --epochs 15
+		    python3 embeddings.py --input_file jcn_brown_thresh01.tsv.gz --vsize ${vsize} --bsize ${bsize} --lrate ${lrate} --vocab_file synsets_vocab.json.gz --use_neighbors True --neighbor_count 3 --epochs 15
 		done
 	    done
     done
