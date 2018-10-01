57acd3b9f992fb59e41f7c6964bf61b767f84e42
Mohammad Dorgham <muhammad.dorgham@gmail.com>
2018-10-01 18:34:19 +0200

    Update trainer-gpu.sh

--- trainer-gpu.sh
+++ trainer-gpu.sh
@@ -4,5 +4,5 @@ vsize=$2
 bsize=$4 
 
 for lrate in 0.005 ; do
-	python embeddings.py ${dataset} ${vsize} ${bsize} ${lrate} synsets_vocab.json.gz
+	python embeddings.py --input_file ${dataset} --vsize ${vsize} --bsize ${bsize} --lrate ${lrate} --vocab_file synsets_vocab.json.gz --use_neighbors True --neighbor_count 3 --epochs 15
 done
